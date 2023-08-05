881626262689996085a2aa47eaad91b3f9f02a5b
Dave Gill <david.g.h.gill@gmail.com>
2023-08-05 17:43:12 +0100

    Added the picking up of stdin - untested

--- src/main.rs
+++ src/main.rs
@@ -87,67 +87,74 @@ async fn main() {
     let _ = swarm.listen_on("/ip4/0.0.0.0/tcp/62649".parse().unwrap());
     log::info!("Listening on port 62649");
 
+    let mut stdin = InteractiveStdin::new();
+
     // Lets catch the swarm events
-    while let Some(event) = swarm.next().await {
-        match event {
-            SwarmEvent::ConnectionEstablished { peer_id, .. } => {
-                log::info!("Connection established with {}", peer_id);
-            }
-            SwarmEvent::ConnectionClosed { peer_id, .. } => {
-                log::info!("Connection with {} closed", peer_id);
-            }
-            SwarmEvent::Behaviour(MyBehaviourEvent::Rendezvous(
-                rendezvous::server::Event::PeerRegistered { peer, registration },
-            )) => {
-                log::info!(
-                    "Peer {} registered for namespace {:?}",
-                    peer,
-                    registration.namespace
-                );
-            }
-            SwarmEvent::Behaviour(MyBehaviourEvent::Rendezvous(
-                rendezvous::server::Event::DiscoverServed {
-                    enquirer,
-                    registrations,
-                },
-            )) => {
-                log::info!(
-                    "Served peer {} with registrations {:?}",
-                    enquirer,
-                    registrations.len()
-                );
-            }
-            SwarmEvent::NewListenAddr { address, .. } => {
-                log::info!("p2p-rendezvous server listening on: {}", address);
-            }
-            SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
-                for (peer_id, _multiaddr) in list {
-                    log::info!("mDNS discovered a new peer: {}", peer_id);
-                    swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
+    loop {
+        tokio::select! {
+            line = stdin.next_line() => {
+                log::info!("Got line: {:?}", line);
+            },
+            event = swarm.select_next_some() => match event {
+                SwarmEvent::ConnectionEstablished { peer_id, .. } => {
+                    log::info!("Connection established with {}", peer_id);
                 }
-            }
-            SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
-                for (peer_id, _multiaddr) in list {
-                    log::info!("mDNS discover peer has expired: {}", peer_id);
-                    swarm
-                        .behaviour_mut()
-                        .gossipsub
-                        .remove_explicit_peer(&peer_id);
+                SwarmEvent::ConnectionClosed { peer_id, .. } => {
+                    log::info!("Connection with {} closed", peer_id);
                 }
-            }
-            SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Message {
-                propagation_source: peer_id,
-                message_id: id,
-                message,
-            })) => {
-                log::info!(
-                    "Got message: '{}' with id: {id} from peer: {peer_id}",
-                    String::from_utf8_lossy(&message.data),
-                );
-            }
-            other => {
-                log::info!("Swarm event: {:?}", other);
-            }
+                SwarmEvent::Behaviour(MyBehaviourEvent::Rendezvous(
+                    rendezvous::server::Event::PeerRegistered { peer, registration },
+                )) => {
+                    log::info!(
+                        "Peer {} registered for namespace {:?}",
+                        peer,
+                        registration.namespace
+                    );
+                }
+                SwarmEvent::Behaviour(MyBehaviourEvent::Rendezvous(
+                    rendezvous::server::Event::DiscoverServed {
+                        enquirer,
+                        registrations,
+                    },
+                )) => {
+                    log::info!(
+                        "Served peer {} with registrations {:?}",
+                        enquirer,
+                        registrations.len()
+                    );
+                }
+                SwarmEvent::NewListenAddr { address, .. } => {
+                    log::info!("p2p-rendezvous server listening on: {}", address);
+                }
+                SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+                    for (peer_id, _multiaddr) in list {
+                        log::info!("mDNS discovered a new peer: {}", peer_id);
+                        swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
+                    }
+                }
+                SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
+                    for (peer_id, _multiaddr) in list {
+                        log::info!("mDNS discover peer has expired: {}", peer_id);
+                        swarm
+                            .behaviour_mut()
+                            .gossipsub
+                            .remove_explicit_peer(&peer_id);
+                    }
+                }
+                SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Message {
+                    propagation_source: peer_id,
+                    message_id: id,
+                    message,
+                })) => {
+                    log::info!(
+                        "Got message: '{}' with id: {id} from peer: {peer_id}",
+                        String::from_utf8_lossy(&message.data),
+                    );
+                }
+                other => {
+                    log::info!("Swarm event: {:?}", other);
+                }
+            } 
         }
     }
 }
@@ -177,5 +184,8 @@ impl InteractiveStdin {
             }
         });
         Self { chan: recv }
+    }
+    async fn next_line(&mut self) -> std::io::Result<Option<String>> {
+        self.chan.recv().await.transpose()
     }
 }
